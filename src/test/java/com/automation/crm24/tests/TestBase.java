09962d717494dae002a8463539a537e1966d60b8
ankarenver <1996ankarjian@gmail.com>
2020-04-06 12:31:53 -0400

    test base added

--- src/test/java/com/automation/crm24/tests/TestBase.java
+++ src/test/java/com/automation/crm24/tests/TestBase.java
@@ -4,20 +4,73 @@ package com.automation.crm24.tests;
 import com.automation.crm24.utilites.BrowserUtils;
 import com.automation.crm24.utilites.ConfigurationReader;
 import com.automation.crm24.utilites.Driver;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
 
 public abstract class TestBase {
+
+    protected WebDriver driver = Driver.getDriver();
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+
+    @BeforeTest
+    public void beforeTest() {
+        extentReports = new ExtentReports();
+        String reportPath = "";
+
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            reportPath = System.getProperty("user.dir") + "\\test-output\\report.html";
+        } else {
+            reportPath = System.getProperty("user.dir") + "/test-output/report.html";
+        }
+        extentHtmlReporter = new ExtentHtmlReporter(reportPath);
+        extentReports.attachReporter(extentHtmlReporter);
+        extentHtmlReporter.config().setReportName("CRM24");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        extentReports.flush();
+    }
+
 
 
     @BeforeMethod
     public void setup(){
-        Driver.getDriver().get(ConfigurationReader.getProperty("environment"));
-        Driver.getDriver().manage().window().maximize();
+        driver.get(ConfigurationReader.getProperty("environment"));
+        driver.manage().window().maximize();
         BrowserUtils.waitForPageLoad(10);
     }
+
     @AfterMethod
-    public void teardown(){
+    public void teardown(ITestResult testResult){
+        if(testResult.getStatus()==ITestResult.FAILURE){
+            String  screenshotLocation = BrowserUtils.getScreenshot(testResult.getName());
+            try {
+                extentTest.fail(testResult.getName());//test name that failed
+                extentTest.addScreenCaptureFromPath(screenshotLocation);//screenshot as an evidence
+                extentTest.fail(testResult.getThrowable());//error message
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to attach screenshot");
+            }
+        }else if(testResult.getStatus() == ITestResult.SUCCESS){
+            extentTest.pass(testResult.getName());
+        }else if(testResult.getStatus() == ITestResult.SKIP){
+            extentTest.skip(testResult.getName());
+        }
+        BrowserUtils.wait(3);
+
         Driver.closeDriver();
     }
 }
--- src/test/java/com/automation/crm24/utilites/BrowserUtils.java
+++ src/test/java/com/automation/crm24/utilites/BrowserUtils.java
@@ -41,25 +41,33 @@ public class BrowserUtils {
      * @param name screenshot name
      * @return path to the screenshot
      */
-    public static String getScreenshot(String name){
-        // adding date and time to screenshot name, to make screenshot unique
-        name = new Date().toString().replace(" ","_").replace(":","_")+"_"+name;
-        // where we gonna store a screenshot
-        String path = System.getProperty("user.dir")+"\\test-output\\screenshots\\"+name+".png";
-        System.out.println("Screenshot is here: "+path);
-        // since our reference type is a WebDriver
-        // we cannot see methods from TakesScreenshot interface
-        // that is why do casting
-        TakesScreenshot takesScreenshot =(TakesScreenshot) Driver.getDriver();
-        // screenshot itself  take screenshot of web browser, and save it as a file
+    public static String getScreenshot(String name) {
+        //adding date and time to screenshot name, to make screenshot unique
+        name = new Date().toString().replace(" ", "_").replace(":", "-") + "_" + name;
+        //where we gonna store a screenshot
+        String path = "";
+
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            path = System.getProperty("user.dir") + "/test-output/screenshots/" + name + ".png";
+        } else {
+            path = System.getProperty("user.dir") + "\\test-output\\screenshots\\" + name + ".png";
+        }
+
+        System.out.println("OS name: " + System.getProperty("os.name"));
+        System.out.println("Screenshot is here: " + path);
+        //since our reference type is a WebDriver
+        //we cannot see methods from TakesScreenshot interface
+        //that's why do casting
+        TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+        //take screenshot of web browser, and save it as a file
         File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
-        // where screenshot will be saved
+        //where screenshot will be saved
         File destination = new File(path);
         try {
-            FileUtils.copyFile(source,destination);
+            //copy file to the previously specified location
+            FileUtils.copyFile(source, destination);
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("File is not found");
         }
         return path;
     }
